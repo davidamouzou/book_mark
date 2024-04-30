8f308d34b1f8d26e1cf3e948c3f87514331519be
David Amouzou <davidamzou.com>
2024-04-30 11:28:11 +0200

    Initial Commit

---
+++ Art Book/Art_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_BookApp.swift
+//  Art Book
+//
+//  Created by David Amouzou on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Art_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Art BookUITests/Art_BookUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Art_BookUITestsLaunchTests.swift
+//  Art BookUITests
+//
+//  Created by David Amouzou on 30/04/2024.
+//
+
+import XCTest
+
+final class Art_BookUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
